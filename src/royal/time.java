5b2d486f79ae86890aa5a2ed3af42913ad19eb25
Pradduman yadav <pradduman.yadav_ccv17@gla.ac.in>
2019-02-26 14:26:18 +0530

    1 commit

---
+++ src/royal/time.java
@@ -0,0 +1,26 @@
+package main;
+
+public class SinglyLinkedList{
+    private class Node;
+    private int data;
+
+    private Node next;
+    public int getNext(){
+        return data;
+
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getData() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
+
+
